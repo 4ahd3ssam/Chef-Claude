fc53d167144630caca02f2871a6beca80260171d
Shahd Essam <112779266+4ahd3ssam@users.noreply.github.com>
2025-12-10 20:42:30 +0200

    Update image source path in Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
     <header className="text-black p-3 flex items-center gap-3 justify-center shadow-[0px_1px_3px_0px_rgba(0,0,0,0.10),_0px_1px_2px_0px_rgba(0,0,0,0.06)]">
       <div className="w-[40px]">
         <img
-          src="src/assets/chef-claude-img-header.png"
+          src="../assets/chef-claude-img-header.png"
           alt="Chef Claude image"
           className="block w-full"
         />
